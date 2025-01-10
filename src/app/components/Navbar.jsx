ac492b2a12fec96c5f62897367bb150df2e73bf5
Admin <wambugudani@gmail.com>
2025-01-10 16:59:07 +0300

    Added DB connection

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -3,9 +3,10 @@ import Link from "next/link"
 const links = [
     { href: "/client", label: "client" },
     { href: "/drinks", label: "drinks" },
+    { href: "/prisma-example", label: "prisma" },
     { href: "/tasks", label: "tasks" },
     { href: "/query", label: "query" },
-    { href: "/prisma-example", label: "prisma-example" },
+    
 ]
 
 const linkElements = links.map(link => (
--- src/app/prisma-example/page.jsx
+++ src/app/prisma-example/page.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import prisma from "../../../utils/db"
 
-const PrismaExamplePage = () => {
+const prismaHandlers = async () => {
+  await prisma.task.create({
+    data:{
+      content: "wake Up"
+    },
+  });
+
+  const allTasks = await prisma.task.findMany({
+    orderBy:{
+      createdAt:"desc"
+    },
+  });
+  return allTasks;
+}
+
+
+const PrismaExamplePage = async () => {
+
+  const tasks = await prismaHandlers()
+
   return (
-    <div>PrismaExamplePage</div>
+    <div>
+      <h1 className="text-7xl">PrismaExamplePage</h1>
+      {
+        tasks.map((task) => {
+          return(
+            <h2 key={task.id} className="text-xl py-2">
+              😁{task.content}
+            </h2>
+          )
+        })
+      }
+    </div>
   )
 }
 
